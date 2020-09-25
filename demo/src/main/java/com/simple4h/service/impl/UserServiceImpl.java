e4a9580c7f8a8461b3ff31dd2ca92433ed1ef21f
Simple4H <crescentcxm@gmail.com>
2020-09-25 15:15:05 +0800

    add redis support

--- demo/src/main/java/com/simple4h/service/impl/UserServiceImpl.java
+++ demo/src/main/java/com/simple4h/service/impl/UserServiceImpl.java
@@ -5,7 +5,10 @@ import com.simple4h.dao.UserMapper;
 import com.simple4h.domain.User;
 import com.simple4h.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * author Create By Simple4H
@@ -17,8 +20,13 @@ public class UserServiceImpl implements IUserService {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @Override
     public ServerResponse<User> getUserInfo(Integer userId) {
+        stringRedisTemplate.opsForValue().set("a", "a");
+        stringRedisTemplate.expire("a", 10L, TimeUnit.SECONDS);
         return ServerResponse.createBySuccess(userMapper.selectByPrimaryKey(userId));
     }
 }
