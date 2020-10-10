e48000da2746ce45e7f4f814f715d4caade7e875
Simple4H <crescentcxm@gmail.com>
2020-10-10 11:27:45 +0800

    1，修改成Nacos 2，添加Gateway

---
+++ gateway/src/test/java/com/simple4h/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simple4h.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
