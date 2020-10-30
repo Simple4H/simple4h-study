60e115fad22ac53fe9a49caa90705efc8187b177
Simple4H <crescentcxm@gmail.com>
2020-10-30 11:10:09 +0800

    自动装配

--- auto-configuration/src/main/java/com/simple4h/autoconfiguration/service/AutoService.java
+++ auto-configuration/src/main/java/com/simple4h/autoconfiguration/service/AutoService.java
@@ -1,22 +1,18 @@
 package com.simple4h.autoconfiguration.service;
 
+import com.simple4h.autoconfiguration.config.AutoBean;
+import org.springframework.beans.factory.annotation.Autowired;
+
 /**
  * author Create By Simple4H
- * date 2020-10-30 10:24
+ * date 2020-10-30 10:51
  */
 public class AutoService {
 
-    public String word;
-
-    public String getWord() {
-        return word;
-    }
-
-    public void setWord(String word) {
-        this.word = word;
-    }
+    @Autowired
+    private AutoBean autoBean;
 
     public String sayWhat() {
-        return "Hello " + word;
+        return "Say What? " + autoBean.getWord();
     }
 }
