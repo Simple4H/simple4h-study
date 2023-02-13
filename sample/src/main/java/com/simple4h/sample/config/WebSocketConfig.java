e0b14f67823586803da7961f98cede35bbdc01c5
Simple4H <crescentcxm@gmail.com>
2023-02-13 14:32:11 +0800

    feat(sample): webSocket support

---
+++ sample/src/main/java/com/simple4h/sample/config/WebSocketConfig.java
@@ -0,0 +1,18 @@
+package com.simple4h.sample.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * WebSocket
+ *
+ * @author Simple4H
+ */
+@Configuration
+public class WebSocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
---
+++ sample/src/main/java/com/simple4h/sample/config/WebSocketServer.java
@@ -0,0 +1,71 @@
+package com.simple4h.sample.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.stream.Collectors;
+
+/**
+ * WebSocketServer
+ *
+ * @author Simple4H
+ */
+@Component
+@Slf4j
+@Service
+@ServerEndpoint("/app/websocket/{userId}")
+public class WebSocketServer {
+
+    private Session session;
+
+    private static final CopyOnWriteArraySet<WebSocketServer> webSocketSet = new CopyOnWriteArraySet<>();
+
+    private Long userId;
+
+    @OnOpen
+    public synchronized void onOpen(Session session, @PathParam("userId") Long userId) {
+        this.session = session;
+        this.userId = userId;
+        webSocketSet.add(this);
+        log.info("建立新的连接,用户Id:{},总数:{}", userId, webSocketSet.size());
+    }
+
+    @OnClose
+    public synchronized void onClose(@PathParam("userId") Long userId) {
+        webSocketSet.remove(this);
+        log.info("连接断开, userId:{}, 总数:{}", userId, webSocketSet.size());
+    }
+
+    @OnMessage
+    public synchronized void onMessage(String message, @PathParam("userId") Long userId) {
+        log.info("接收到userId:{}信息,内容:{}", userId, message);
+    }
+
+    public static synchronized void sendMessage(Long userId, String message) {
+        if (Objects.isNull(userId)) {
+            throw new RuntimeException("用户Id不能为空");
+        }
+
+        List<WebSocketServer> webSocketServers = webSocketSet.stream().filter(s -> s.userId.equals(userId)).collect(Collectors.toList());
+        webSocketServers.forEach(server -> {
+            try {
+                server.session.getBasicRemote().sendText(message);
+            } catch (IOException e) {
+                log.error("发送信息异常,userId:{}, message:{}", userId, message);
+                throw new RuntimeException("发送信息异常！");
+            }
+        });
+    }
+
+}
