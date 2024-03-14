0d3c45345b585b58ba8416d91893f9ed915b3c6f
Simple4H <crescentcxm@gmail.com>
2024-03-14 16:09:56 +0800

    feat(all): init spring boot3

--- simple4h-demo/src/main/java/com/simple4h/demo/Simple4hDemoApplication.java
+++ simple4h-demo/src/main/java/com/simple4h/demo/Simple4hDemoApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.simple4h.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Simple4hDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(Simple4hDemoApplication.class, args);
     }
 
 }
--- simple4h-demo/src/test/java/com/simple4h/demo/Simple4hDemoApplicationTests.java
+++ simple4h-demo/src/test/java/com/simple4h/demo/Simple4hDemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.simple4h.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class Simple4hDemoApplicationTests {
 
     @Test
     void contextLoads() {
