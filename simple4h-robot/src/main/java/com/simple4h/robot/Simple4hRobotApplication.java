cd363c30e5a0a9330db271fb3d05918cfad983b2
Simple4H <crescentcxm@gmail.com>
2024-03-25 11:06:20 +0800

    feat(all): dingtalk support

---
+++ simple4h-robot/src/main/java/com/simple4h/robot/Simple4hRobotApplication.java
@@ -0,0 +1,13 @@
+package com.simple4h.robot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Simple4hRobotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Simple4hRobotApplication.class, args);
+    }
+
+}
