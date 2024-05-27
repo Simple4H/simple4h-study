62a881fd2c92247fb60717fde664f71dea957a94
Simple4H <crescentcxm@gmail.com>
2024-05-27 11:47:42 +0800

    feat(all): ListTypeHandle

---
+++ simple4h-base/src/main/java/com/simple4h/base/config/ListTypeHandler.java
@@ -0,0 +1,59 @@
+package com.simple4h.base.config;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.TypeReference;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.MappedJdbcTypes;
+import org.apache.ibatis.type.MappedTypes;
+import org.springframework.util.CollectionUtils;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * ListTypeHandle
+ *
+ * @author Simple4H
+ */
+@MappedJdbcTypes(JdbcType.ARRAY)
+@MappedTypes({List.class})
+public abstract class ListTypeHandler<T> extends BaseTypeHandler<List<T>> {
+
+    @Override
+    public void setNonNullParameter(PreparedStatement ps, int i, List<T> parameter, JdbcType jdbcType) throws SQLException {
+        String content = CollectionUtils.isEmpty(parameter) ? null : JSON.toJSONString(parameter);
+        ps.setString(i, content);
+    }
+
+    @Override
+    public List<T> getNullableResult(ResultSet rs, String columnName) throws SQLException {
+        return this.getListByJsonArrayString(rs.getString(columnName));
+    }
+
+    @Override
+    public List<T> getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+        return this.getListByJsonArrayString(rs.getString(columnIndex));
+    }
+
+    @Override
+    public List<T> getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+        return this.getListByJsonArrayString(cs.getString(columnIndex));
+    }
+
+    private List<T> getListByJsonArrayString(String content) {
+        return StringUtils.isBlank(content) ? new ArrayList<>() : JSON.parseObject(content, this.specificType());
+    }
+
+    /**
+     * 具体类型，由子类提供
+     *
+     * @return 具体类型
+     */
+    protected abstract TypeReference<List<T>> specificType();
+}
